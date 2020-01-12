1466095121317bbb1b282705e9134e1f89543bc2
Juan Carlos Hoyos <jcabarique@uninorte.edu.co>
2020-01-11 19:35:15 -0500

    Se ha agregado la navegacion a la pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app/app.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent
+    BannerComponent,
+    NavegacionComponent
   ],
   imports: [
     BrowserModule
